c157e646ad4a0967c79910beb8a18e9f46c0044a
alexpryp <alexpryp@gmail.com>
2020-02-11 11:43:09 +0200

    Added salary chart

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+
 const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 const salar2019 = [9223, 9429, 10237, 10269, 10239, 10783, 10971, 10537, 10687, 10727, 10679, 12264];
 
@@ -53,6 +59,140 @@ class App extends React.Component {
         );
     }
 }
+
+
+/* let data =[{
+    "country": "USA",
+    "visits": 4025
+  }, {
+    "country": "China",
+    "visits": 1882
+  }, {
+    "country": "Japan",
+    "visits": 1809
+  }, {
+    "country": "Germany",
+    "visits": 1322
+  }, {
+    "country": "UK",
+    "visits": 1122
+  }, {
+    "country": "France",
+    "visits": 1114
+  }, {
+    "country": "India",
+    "visits": 984
+  }, {
+    "country": "Spain",
+    "visits": 711
+  }, {
+    "country": "Netherlands",
+    "visits": 665
+  }, {
+    "country": "Russia",
+    "visits": 580
+  }, {
+    "country": "South Korea",
+    "visits": 443
+  }, {
+    "country": "Canada",
+    "visits": 441
+  }, {
+    "country": "Brazil",
+    "visits": 395
+  }, {
+    "country": "Italy",
+    "visits": 386
+  }, {
+    "country": "Australia",
+    "visits": 384
+  }, {
+    "country": "Taiwan",
+    "visits": 338
+  }, {
+    "country": "Poland",
+    "visits": 328
+  }]; */
+
+
+
+  function createDataArray (months, salary) {
+    let array = [];
+
+    for (let i = 0; i < months.length; i++) {
+        let obj = {
+            "month": months[i],
+            "salary": salary[i]
+        };
+
+        array.push(obj);
+    }
+
+    return array;
+  }
+
+  let data = createDataArray(months, salar2019);
+
+
+
+
+/* am4core.ready( */
+    function buildChart(data) {
+
+    // Themes begin
+    am4core.useTheme(am4themes_dark);
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    // Create chart instance
+    var chart = am4core.create("chartdiv", am4charts.XYChart3D);
+
+    // Add data
+    chart.data = data;
+    
+    // Create axes
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "month";
+    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.renderer.labels.template.hideOversized = false;
+    categoryAxis.renderer.minGridDistance = 20;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.verticalCenter = "middle";
+    categoryAxis.tooltip.label.rotation = 270;
+    categoryAxis.tooltip.label.horizontalCenter = "right";
+    categoryAxis.tooltip.label.verticalCenter = "middle";
+    
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.title.text = "Зарплата грн./мес.";
+    valueAxis.title.fontWeight = "bold";
+    
+    // Create series
+    var series = chart.series.push(new am4charts.ColumnSeries3D());
+    series.dataFields.valueY = "salary";
+    series.dataFields.categoryX = "month";
+    series.name = "Visits";
+    series.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = .8;
+    
+    var columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+    columnTemplate.stroke = am4core.color("#FFFFFF");
+    
+    columnTemplate.adapter.add("fill", function(fill, target) {
+      return chart.colors.getIndex(target.dataItem.index);
+    })
+    
+    columnTemplate.adapter.add("stroke", function(stroke, target) {
+      return chart.colors.getIndex(target.dataItem.index);
+    })
+    
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.lineX.strokeOpacity = 0;
+    chart.cursor.lineY.strokeOpacity = 0;
+}
+/*     );  */
+buildChart(data);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
